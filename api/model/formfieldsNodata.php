a87e7a3ab0a098c124514cf3e762af9ff70d0f91
gradile@cox.net <gradile@cox.net>
2021-02-11 23:11:19 -0700

    changes to fix error on response

--- api/model/formfieldsNodata.php
+++ api/model/formfieldsNodata.php
@@ -2,12 +2,13 @@
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\SMTP;
-  
+  require '../vendor/autoload.php';
+
 class FormFields {
     public $subject;
-    public $name;
-    public $email;
-    public $query;
+    public $contact_name;
+    public $contact_email;
+    public $contact_question;
 
     public function sendEmailForm() {
       
@@ -15,53 +16,52 @@ class FormFields {
         //This should be done in your php.ini, but this is how to do it if you don't have access to that
         date_default_timezone_set('Etc/UTC');
 
-        require '../vendor/autoload.php';
+       
         $this->subject=htmlspecialchars(strip_tags($this->subject));
-        $this->name=htmlspecialchars(strip_tags($this->name));
-        $this->email=htmlspecialchars(strip_tags($this->email));
-        $this->query=htmlspecialchars(strip_tags($this->query));
+        $this->contact_name=htmlspecialchars(strip_tags($this->contact_name));
+        $this->contact_email=htmlspecialchars(strip_tags($this->contact_email));
+        $this->contact_question=htmlspecialchars(strip_tags($this->contact_question));
                 
-                //Create a new PHPMailer instance
-                $mail = new PHPMailer();
-                //Tell PHPMailer to use SMTP
-                $mail->isSMTP();
-                //Enable SMTP debugging
-                // SMTP::DEBUG_OFF = off (for production use)
-                // SMTP::DEBUG_CLIENT = client messages
-                // SMTP::DEBUG_SERVER = client and server messages
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-                //Set the hostname of the mail server
-                $mail->Host = 'smtp.myserver.com';
-                //Set the SMTP port number - likely to be 25, 465 or 587
-                $mail->Port = 25;
-                //Whether to use SMTP authentication
-                $mail->SMTPAuth = true;
-                //Username to use for SMTP authentication
-                $mail->Username = 'username';
-                //Password to use for SMTP authentication
-                $mail->Password = 'password';
-                //Set who the message is to be sent from
-                $mail->setFrom('info@example.com', 'Company Name');
-                //Set an alternative reply-to address
-                $mail->addReplyTo('info@example.com', 'Company Name');
-                //Set who the message is to be sent to
-                $mail->addAddress('john@example.com', 'John Doe');
-                //Set the subject line
-                $mail->Subject = 'PHPMailer SMTP test';
-                //Read an HTML message body from an external file, convert referenced images to embedded,
-                //convert HTML into a basic plain-text alternative body
-                // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-                //Replace the plain text body with one created manually
-                $mail->AltBody = 'This is a plain-text message body';
-                //Attach an image file
-                $mail->addAttachment('images/phpmailer_mini.png');
-                $mail->Subject = 'Contact form: ' . $this->subject;
-                $mail->Body = '<div>Name: ' . $this->name .'</div><div><div>Email: ' .$this->email . '</div><div>Question: ' . $this->query . '</div>';
-                if (!$mail->send()) {
-                    echo('Mailer Error: ' . $mail->ErrorInfo);
-                } else {
-                    echo('Message sent!');
-                }
-            }
-        
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer();
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+        //Enable SMTP debugging
+        // SMTP::DEBUG_OFF = off (for production use)
+        // SMTP::DEBUG_CLIENT = client messages
+        // SMTP::DEBUG_SERVER = client and server messages
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        //Set the hostname of the mail server
+        $mail->Host = 'smtp.myserver.com';
+        //Set the SMTP port number - likely to be 25, 465 or 587
+        $mail->Port = 25;
+        //Whether to use SMTP authentication
+        $mail->SMTPAuth = true;
+        //Username to use for SMTP authentication
+        $mail->Username = 'email@server.com';
+        //Password to use for SMTP authentication
+        $mail->Password = 'password';
+        //Set who the message is to be sent from
+        $mail->setFrom('info@example.com', 'My Company Name');
+        //Set an alternative reply-to address
+        $mail->addReplyTo('info@example.com', 'My Company Name');
+        //Set who the message is to be sent to
+        $mail->addAddress('user@example.com', 'John Doe');
+        //Set the subject line
+        //$mail->Subject = 'PHPMailer SMTP test';
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+        //Replace the plain text body with one created manually
+        $mail->AltBody = 'This is a plain-text message body';
+        //Attach an image file
+        //$mail->addAttachment('images/phpmailer_mini.png');
+        $mail->Subject = 'Contact form: ' . $this->subject;
+        $mail->Body = '<div>Name: ' . $this->contact_name .'</div><div><div>Email: ' .$this->contact_email . '</div><div>Question: ' . $this->contact_question . '</div>';
+        if (!$mail->send()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- api/smtpEmail.php
+++ api/smtpEmail.php
@@ -1,10 +1,8 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-dataed-With");
     include_once './model/formfields.php';
 
     $item = new FormFields();
@@ -16,10 +14,10 @@
     $item->contact_email = $data->contact_email;
     $item->contact_question = $data->contact_question;
   
-    
+   // $item->sendEmailForm();
     if($item->sendEmailForm()){
-        echo 'Email created successfully.';
+        echo  json_encode('Email created successfully.');
     } else{
-        echo 'Email could not be created.';
+        echo json_encode('Email could not be created.');
     }
 ?>
